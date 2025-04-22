8a0ba2e4ff56dbb4839d2bcd69a7cace22a227b3
Sergei Krasilnikov <s.krasilnikov@intiled.ru>
2025-04-22 11:49:02 +0300

    fix sample

--- Samples/Program.cs
+++ Samples/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 
 namespace Haukcode.ArtNet.Samples;
 
@@ -6,27 +7,53 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("ArtNet samples!");
+        Console.WriteLine("Welcome to ArtNet samples!");
 
         var addr = Haukcode.Network.Utils.GetFirstBindAddress();
 
-        // using (var tester = new ArtTriggerSender(localIp: addr.IPAddress, localSubnetMask: addr.NetMask))
-        // {
-        //     Console.WriteLine("Hit enter to send select/play command...");
-        //     Console.ReadLine();
-        //
-        //     byte showId = 1;
-        //     // Select show
-        //     tester.SendArtTrigger(oemCode: 0x6A6B, key: 0x03, subKey: showId);
-        //
-        //     // Playback
-        //     tester.SendArtTrigger(oemCode: 0x6A6B, key: 0x02, subKey: 0x47);
-        //
-        //
-        //     Console.WriteLine("Hit enter to exit...");
-        //     Console.ReadLine();
-        // }
+        do
+        {
+            Console.WriteLine("Choose sample:");
+            Console.WriteLine("\t1 - ArtTriggerSample");
+            Console.WriteLine("\t2 - RdmSample");
+            Console.WriteLine("\t0 - Exit");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    ArtTriggerSample(addr);
+                    break;
+                case "2":
+                    RdmSample(addr);
+                    break;
+                case "0":
+                    Console.WriteLine("Bye...");
+                    return;
+            }
+           
+        } while (true);
+    }
 
+    private static void ArtTriggerSample((IPAddress IPAddress, IPAddress NetMask, byte[] MacAddress) addr)
+    {
+        using (var tester = new ArtTriggerSender(localIp: addr.IPAddress, localSubnetMask: addr.NetMask))
+        {
+            Console.WriteLine("Hit enter to send select/play command...");
+            Console.ReadLine();
+        
+            byte showId = 1;
+            // Select show
+            tester.SendArtTrigger(oemCode: 0x6A6B, key: 0x03, subKey: showId);
+        
+            // Playback
+            tester.SendArtTrigger(oemCode: 0x6A6B, key: 0x02, subKey: 0x47);
+            
+            Console.WriteLine("Hit enter to exit...");
+            Console.ReadLine();
+        }
+    }
+
+    private static void RdmSample((IPAddress IPAddress, IPAddress NetMask, byte[] MacAddress) addr)
+    {
         using (var tester = new RdmSample(localIp: addr.IPAddress, localSubnetMask: addr.NetMask))
         {
             Console.WriteLine("Sending single ArtPollPacket...");
@@ -40,6 +67,7 @@ public class Program
 
             tester.GetDeviceLabels();
             
+            Console.WriteLine("Hit enter to exit...");
             Console.ReadLine();
         }
     }
